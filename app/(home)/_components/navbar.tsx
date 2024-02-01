9e7758ca21f19a5652d9ea7bb5d8b1d14e1ff050
thenishantgiri <nishantkrgiri@gmail.com>
2024-02-01 16:06:30 +0530

    Home: Navbar button link fixed

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex justify-between items-center w-full">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
@@ -16,6 +16,6 @@ export const Navbar = () => {
           </Button>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
