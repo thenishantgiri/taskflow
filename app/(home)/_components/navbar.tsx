133fcb8c18e489ccea27b3c292aceb0f35788040
thenishantgiri <nishantkrgiri@gmail.com>
2024-02-01 14:18:51 +0530

    Authentication: Clerk Added

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button size={"sm"} variant={"outline"} asChild>
-            <Link href="/login">Login</Link>
+            <Link href="/sign-in">Login</Link>
           </Button>
           <Button size={"sm"} asChild>
             <Link href={"/sign-up"}>Get Started</Link>
